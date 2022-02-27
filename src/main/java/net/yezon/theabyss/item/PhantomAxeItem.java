6e2847ad9d297676b1e807e0e1abff56fc135bd2
♠ჯἶõῆἶõჯ♠ <86433158+XionioXMaster@users.noreply.github.com>
2022-02-27 13:39:48 +0300

    Update PhantomAxeItem.java

--- src/main/java/net/yezon/theabyss/item/PhantomAxeItem.java
+++ src/main/java/net/yezon/theabyss/item/PhantomAxeItem.java
@@ -47,7 +47,7 @@ public class PhantomAxeItem extends TheAbyss.Processor {
 			public Ingredient getRepairMaterial() {
 				return Ingredient.fromStacks(new ItemStack(PhantomIngotItem.block));
 			}
-		}, 1, -3.6f, new Item.Properties().group(TheAbyssToolsItemGroup.tab)) {
+		}, 1, -3.1f, new Item.Properties().group(TheAbyssToolsItemGroup.tab)) {
 		}.setRegistryName("phantom_axe"));
 	}
 }
