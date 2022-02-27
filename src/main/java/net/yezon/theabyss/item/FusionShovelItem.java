57857fd4d4e44b93e7a9a6443be6a388021076a3
♠ჯἶõῆἶõჯ♠ <86433158+XionioXMaster@users.noreply.github.com>
2022-02-27 11:37:42 +0300

    Update FusionShovelItem.java

--- src/main/java/net/yezon/theabyss/item/FusionShovelItem.java
+++ src/main/java/net/yezon/theabyss/item/FusionShovelItem.java
@@ -47,7 +47,7 @@ public class FusionShovelItem extends TheAbyss.Processor {
 			public Ingredient getRepairMaterial() {
 				return Ingredient.fromStacks(new ItemStack(FusionIngotItem.block));
 			}
-		}, 1, -1f, new Item.Properties().group(TheAbyssToolsItemGroup.tab)) {
+		}, 1, -3f, new Item.Properties().group(TheAbyssToolsItemGroup.tab)) {
 		}.setRegistryName("fusion_shovel"));
 	}
 }
